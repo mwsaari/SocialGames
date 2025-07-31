ff576b5751f6aae9ea77980b411b6086cf0ed76e
Matthew Saari <saari.matthew@gmail.com>
2025-07-31 18:33:15 -0500

    Adding games list

---
+++ Controllers/GamesController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SocialGames.Models;
+using SocialGames.Models.Games;
+
+namespace SocialGames.Controllers
+{
+    public class GamesController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewData["Games"] = new List<IGame>()
+            {
+                new SecretSocieties(),
+                new OneNight(),
+            };
+            return View();
+        }
+    }
+}
---
+++ Models/Games/OneNight.cs
@@ -0,0 +1,10 @@
+﻿namespace SocialGames.Models.Games
+{
+    public class OneNight : IGame
+    {
+        public string Name => "One Night";
+        public string Description => "A fast-paced social deduction game where players take on roles and try to deduce who among them is the werewolf, vampire, or other hidden roles within a single night.";
+        public int MinPlayers => 3;
+        public int MaxPlayers => 10;
+    }
+}
